e14a1ab999ba9c43b2e2e83de2326fa7c072ef64
Tanvi <tanvi.avula@outlook.com>
2020-08-24 15:20:48 +0530

    Added new api

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return "Hello world!"
 
+@app.route("/")
+def index():
+    return "Welcome to flask app"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
